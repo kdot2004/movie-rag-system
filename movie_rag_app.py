e0311dd8c512dd3aaa5c20d321d1b46c79d7fd76
kdot2004 <krayuk99@gmail.com>
2026-04-17 00:25:10 -0500

    First version of the movie rag app. Set up currently that answer generation won't work.

--- movie_rag_app.py
+++ movie_rag_app.py
@@ -19,24 +19,27 @@ embedding_model = load_embedding_model("all-mpnet-base-v2")
 #tokenizer, llm = load_llm_and_tokenizer("mistralai/Mistral-7B-Instruct-v0.3")
 
 
+# Format background
+st.markdown(
+    """
+    <style>
+    .stTextInput>div>input {
+        background-color: #f0f0f0;
+        color: #333;
+    }
+    </style>
+    """, unsafe_allow_html=True)
+
 # Create UI
 st.title("🎬🎥 Movie RAG System 🍿🎟️ ")
-st.caption("An AI Chatbot designed to answer all your questions about movies! 🤖")
+st.caption("An AI Chatbot designed to answer all your questions about movies!")
 
-
-# Load movies
-with open("movie_titles.txt", "r", encoding="utf-8") as f:
-    movies = [line.strip() for line in f]
-# Display avaliable movie data: https://docs.streamlit.io/develop/api-reference/widgets/st.selectbox
-
-# Store the initial value of widgets in session state
-st.session_state.disabled = False
 
 # Create side search bar
 # https://docs.streamlit.io/develop/api-reference/layout/st.sidebar
 with st.sidebar:
     st.sidebar.title("🚨 Movies Avaliable in Database 🚨")
-    title_query = st.text_input("Search for a movie title:")
+    title_query = st.text_input("Search for a movie title 🔻:")
     num_movies = st.slider("How many movies to retrieve?", 1, 10) # https://docs.streamlit.io/develop/api-reference/widgets/st.slider
     if st.button("Find Movie"): 
         if title_query:
@@ -45,29 +48,52 @@ with st.sidebar:
             for i, movie in enumerate(retrieved_movies, start=1):
                 st.write(f"{i}. {movie}")
 
-col1, col2 = st.columns(2)
-with col1:
-    st.checkbox("Disable selectbox widget", key="disabled")
-with col2:
-    movie_options = st.selectbox(
-                "Movies in Database 🔻",
-                (movies)
-            )
-    disabled=st.session_state.disabled
+# Initialize chat history
+# https://github.com/streamlit/llm-examples/blob/main/Chatbot.py
+# https://mahapatra-preetam.medium.com/building-a-conversational-ai-with-memory-in-streamlit-using-langgraph-langchain-asyncio-and-96841a038fb5
+if "messages" not in st.session_state:
+    st.session_state.messages = []
 
+# Display a welcome message if the chat is empty
+if not st.session_state.messages:
+    with st.chat_message("assistant"):
+        st.write("Hello, I am your personal movie Q&A assistant! Ask away! 🤖")
 
+# Display chat messages from history on app rerun
+for message in st.session_state.messages:
+    with st.chat_message(message["role"]):
+        st.markdown(message["content"])
 # Chat Interface: https://docs.streamlit.io/develop/api-reference/chat/st.chat_input
 query = st.text_input("Enter your query:", "")
 
 if st.button("Get Answer"):
-    with st.spinner("Generating Response...Please Wait"):
-        if query:
+    if query:
+        # Display user message in chat message container
+        with st.chat_message("user"):
+            st.markdown(query)
+
+        # Add user message to chat history for immediate display
+        st.session_state.messages.append({"role": "user", "content": query})
+        # Show spinner widget while response is being generated
+        with st.spinner("Generating Answer... Please Be Patient"):
+            try:
                 # Call the RAG pipeline function here
                 #answer = rag_pipeline(query, collection, embedding_model, model, tokenizer, top_k=5)
                 #st.write(textwrap.fill(answer, width=80))
+                answer = "Hellooo Worldddd"
                 pass
-    #else:
-        #st.write("Please enter a query.")
+            except Exception as e:
+                st.write("Error running query: {e}")
+                answer = "Sorry I ran into a unexpected error, please try again!"
 
-# https://streamlit.io/playground
-# https://streamlit.io/gallery?category=llms
+        # Display assistant response in chat message container
+        with st.chat_message("assistant"):
+            st.markdown(answer)
+
+         # Add assistant response to chat history
+        st.session_state.messages.append({"role": "assistant", "content": answer})
+    else:
+        st.write("Please enter a query.")
+
+    # https://streamlit.io/playground
+    # https://streamlit.io/gallery?category=llms
